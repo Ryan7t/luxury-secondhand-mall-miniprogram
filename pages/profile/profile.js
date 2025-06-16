e21efb16a2f8b33c2548d2d3463f694823204375
Ethan <2368077712@qq.com>
2025-06-16 17:06:59 +0800

    初始化提交二手奢侈品小程序项目

---
+++ pages/profile/profile.js
@@ -0,0 +1,76 @@
+// pages/profile/profile.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentTab: 0 // 默认选中第一个标签（待付款）
+  },
+
+  /**
+   * 标签切换事件处理函数
+   */
+  switchTab(e) {
+    const index = e.currentTarget.dataset.index;
+    this.setData({
+      currentTab: index
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
